74e1ad58b3e007ee2740b3cf971cec2c2661cb32
vkhari18 <126150041@sastra.ac.in>
2026-02-07 22:47:19 +0530

    Update Nandhu.py

--- Nandhu.py
+++ Nandhu.py
@@ -227,8 +227,9 @@ else:
         unsafe_allow_html=True,
     )
 
-show_gif("love-proposal-ajith-kumar.gif")
-show_gif("ajithgifs-ajithkumar.gif")
-show_gif("gesture-kiss.gif")
+    show_gif("love-proposal-ajith-kumar.gif")
+    show_gif("ajithgifs-ajithkumar.gif")
+    show_gif("gesture-kiss.gif")
+
 
 
